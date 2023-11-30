1555c7cb14d4103781c109673d32d4ad83710f58
Reshmacheck <reshmacheck93600@gmail.com>
2023-11-30 16:12:12 +0100

    authentification 2

--- client/src/components/header/HeaderVisitor.jsx
+++ client/src/components/header/HeaderVisitor.jsx
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom'
 
 
 const HeaderVisitor = () => {
-    return <>
+  return <>
+      
           <header className="header">
           <nav>
             <div className="nav">
